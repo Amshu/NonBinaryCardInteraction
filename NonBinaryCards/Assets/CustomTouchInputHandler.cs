0a4fd5d3913cd4e26699e88ead52ec30e46f5242
DML_PC <50893358+CUBE-Deakin-University@users.noreply.github.com>
2021-05-24 13:35:53 +1000

    Added CustomTouchInputHandler to recognize of touch is on top or bottom half of the screen.

---
+++ NonBinaryCards/Assets/CustomTouchInputHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+using Lean.Touch;
+
+public class CustomTouchInputHandler : MonoBehaviour
+{
+    private int screenHeight = 0;
+
+    private void Awake()
+    {
+        // et the height of the screen
+        screenHeight = Screen.height;
+    }
+
+    public void OnFingerSwipe(LeanFinger finger)
+    {
+        int screenY = (int)finger.StartScreenPosition.y;
+
+        // Check if the swipe was on top or bottom half of the screen
+        if (screenY > screenHeight * 0.5)
+        {
+            Debug.Log( "Top");
+            
+            // Change top screen object
+        }
+        else
+        {
+            Debug.Log("Down");
+            
+            // Change bottom screen object
+        }
+    }
+}
